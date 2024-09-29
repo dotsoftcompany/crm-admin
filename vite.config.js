88d8ba020f0fb4822e83ec4c48cb6b16e1069406
Mukhammadxuja Anvarov <87266381+mukhammadxuja@users.noreply.github.com>
2024-09-29 13:43:29 +0500

    add react hook form && localhost:3000 as a default

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,10 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: "localhost",
+    port: 3000,
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
