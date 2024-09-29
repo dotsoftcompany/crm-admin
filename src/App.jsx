e893f1f36d886a307519d31058c1555aed92a278
Mukhammadxuja Anvarov <87266381+mukhammadxuja@users.noreply.github.com>
2024-09-29 13:45:31 +0500

    fix: main max-width

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,3 @@
-import { ThemeProvider } from '@/provider/ThemeProvider';
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
-import { ThemeToggle } from '@/components/ThemeToggle';
-
-import './App.css';
 import DashboardLayout from './components/dashboard/layout';
 
 function App() {
